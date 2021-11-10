0d3fbd648e9d9b33950a5b7ddfa53922a67ca983
cola639 <337507950@qq.com>
2021-11-11 01:53:15 +0800

    add eventLoop

---
+++ eventLoop/eventLoop0.js
@@ -0,0 +1,14 @@
+//sync mission
+function func1() {
+  console.log(1);
+}
+
+function func2() {
+  console.log(2);
+  func1();
+  console.log(3);
+}
+
+func2();
+
+console.log("***************split**********");
---
+++ eventLoop/eventLoop1.js
@@ -0,0 +1,14 @@
+//setTimeout()
+function func1() {
+  console.log(1);
+}
+
+function func2() {
+  setTimeout(() => {
+    console.log(2);
+  }, 0);
+  func1();
+  console.log(3);
+}
+
+func2();
