07a028b2f625bbf781f0bae48296d46884eaa2f8
cola639 <337507950@qq.com>
2022-05-02 03:29:23 +0800

    array

---
+++ array/array_reduce.js
@@ -0,0 +1,45 @@
+// grammer
+// arr.reduce((accumulator,item,index,array)=>{},initvalue)
+
+// 1 accumulation
+const arr = [{ x: 1 }, { x: 2 }, { x: 3 }]
+
+const sum = arr.reduce(
+  (accumulator, currentValue) => accumulator + currentValue.x,
+  0
+)
+console.log(`accumulation ${sum}`)
+
+// 2 deduplication
+// 思路:
+// 1.先设置空对象标识,空数组装载
+// 2.判断当前obj[key]是否添加,是跳过不处理,否设为操作过trur 添加进数组
+// 3.返回累计数组
+const users = [
+  { id: 0, name: '小明' },
+  { id: 1, name: '小张' },
+  { id: 1, name: '小周' },
+  { id: 2, name: '小陈' },
+  { id: 2, name: '小李' },
+  { id: 3, name: '小孙' },
+]
+
+const obj = {} // 空对象标识重复值
+const deduplicationUser = users.reduce((accumulator, item) => {
+  obj[item.id] ? null : (obj[item.id] = true && accumulator.push(item))
+  return accumulator
+}, [])
+console.log(deduplicationUser)
+
+// 3 flatten
+// 思路：
+// 判断是否为数组 递归遍历
+const beforeFlatten = [[1, [2, 3]], 4, 5]
+
+function flatten(beforeFlatten) {
+  return beforeFlatten.reduce((accumulator, item) => {
+    return accumulator.concat(Array.isArray(item) ? flatten(item) : item)
+  }, [])
+}
+
+console.log(`flatten ${flatten(beforeFlatten)}`)
--- array/ascending.js
+++ array/ascending.js
@@ -1,5 +1,5 @@
-// var arr = [5, 3, 6, 2, 8, 10];
-const arr = [6, 5, 3, 0];
+// const  arr = [5, 3, 6, 2, 8, 10];
+const arr = [6, 5, 3, 0]
 
 //1 两次for循环 2 采取中间变量进行位移 3 第二层循环j=0,意味着每次从改变后数组第0位开始
 
@@ -8,12 +8,12 @@ for (i = 0; i < arr.length - 1; i++) {
   for (j = 0; j < arr.length - 1; j++) {
     if (arr[j + 1] < arr[j]) {
       //调换位置
-      console.log("进来时", arr);
-      change = arr[j + 1];
-      arr[j + 1] = arr[j];
-      arr[j] = change;
-      console.log("操作后", arr);
+      console.log('进来时', arr)
+      change = arr[j + 1]
+      arr[j + 1] = arr[j]
+      arr[j] = change
+      console.log('操作后', arr)
     }
   }
 }
-console.log(arr);
+console.log(arr)
