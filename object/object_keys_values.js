265a168d15b7f301098c7f4dff5b9f75ad0a81f5
cola639 <337507950@qq.com>
2023-02-11 00:57:34 +0800

    add

---
+++ object/object_keys_values.js
@@ -0,0 +1,5 @@
+const data = { a: 1 }
+const keyArr = Object.keys(data)
+const valueArr = Object.values(data)
+console.log(keyArr, keyArr.length) // 0 empty object
+console.log(valueArr, valueArr.length) // 0 empty object
