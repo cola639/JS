9d178246679b945f18251fdd4a1fadb8e80b9a81
cola639 <337507950@qq.com>
2023-03-30 16:49:25 +0800

    add

--- Windows/onresize/index.js
+++ Windows/onresize/index.js
@@ -2,8 +2,7 @@
 
 // debounce 防抖
 // 触发事件后在n秒内函数只能执行一次，如果在n秒内又触发了事件，则会重新计算函数执行时间。
-// 场景: 1 防止搜索框频繁请求 2 手机号,邮箱频繁验证 3 window.resize
-// Example1 window.addEventListener('resize', debounce(function, 3 * 1000))
+// 场景: 1 防止搜索框输入时频繁发起请求  2 window.resize Echart变化 3 手机号,邮箱频繁验证
 
 /**
  *
