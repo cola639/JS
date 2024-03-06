5f9c04c649d67b59c884537598c4aa02940ee5a1
cola639 <337507950@qq.com>
2024-03-06 10:47:59 +0800

    update

--- math/basic/index.js
+++ math/basic/index.js
@@ -1,18 +1,21 @@
-// Math.random()
-const res = Math.random()
-console.log('res >> ', res) // Output: 1 > res >= 0
+// 1 Math.random()
+const randomRes = Math.random()
+console.log('randomRes >> ', randomRes) // Output: 1 > res >= 0
 
-// Math.floor() 向下取整 不遵循四舍五入
+// 2 Math.floor() 向下取整 不遵循四舍五入
 const floorRes = Math.floor(9.9)
 console.log('floorRes >> ', floorRes) // Output: 9
 
-// Math.ceil() 向上取整  遵循四舍五入
+// 3 Math.ceil() 向上取整  遵循四舍五入
 const ceilRes = Math.ceil(9.1)
 console.log('ceilRes >> ', ceilRes) // Output: 10
 
-// 合并使用 生成10内包括0-9
+// 4 生成10内包括0-9
 const zeroToNine = Math.floor(Math.random() * 10)
-console.log('zeroToNine >> ', zeroToNine) // Output: 0-9
+console.log('0-9 >> ', zeroToNine) // Output: 0-9
 
-// Infinity 无限大
+// 5 Infinity 无限大
 const max = Infinity
+
+// 6 -Infinity 无限小
+const min = -Infinity
