1f85ddb8103992a1743a39ccdef3ff9369ee2882
cola639 <337507950@qq.com>
2024-03-11 17:53:56 +0800

    update

--- array/sort.js
+++ array/sort.js
@@ -1,19 +1,24 @@
-//reverse
-const arr = [5, 4, 3, 2, 1];
-console.log(arr.reverse());
+// reverse
+const arr = [5, 4, 3, 2, 1]
+console.log(arr.reverse())
 
-//sort
-const arr1 = [0, 1, 5, 10, 15];
+// sort
+const arr1 = [0, 1, 5, 10, 15]
 
 function compare(value1, value2) {
   if (value1 < value2) {
-    return -1;
+    return -1
   } else if (value1 > value2) {
-    return 1;
+    return 1
   } else {
-    return 0;
+    return 0
   }
 }
 
-console.log(arr1.sort()); //ASII code
-console.log(arr1.sort(compare));
+console.log(arr1.sort()) //ASII code
+console.log(arr1.sort(compare))
+
+// 用比较函数来确保数字按大小排序
+let numbers = [11, 15, 2, 7]
+numbers.sort((a, b) => a - b)
+console.log(numbers) // [2, 7, 11, 15]
--- map/index.js
+++ map/index.js
@@ -1,32 +1,28 @@
 const map1 = new Map()
 
+// 'set'
 map1.set('a', 1)
 map1.set('b', 2)
 map1.set('c', 3)
 
+// 'has'
 hasKey = map1.has('a')
 console.log('has ', hasKey)
-
-console.log(map1)
-// Expected output: 1
+console.log(map1) // Expected output: 1
 
 map1.set('a', 97)
 map1.get('a')
-console.log(map1.get('a'))
-// Expected output: 97
+console.log(map1.get('a')) // Expected output: 97
 
-console.log(map1.size)
-// Expected output: 3
+console.log(map1.size) // Expected output: 3
 
 map1.delete('b')
 
-console.log(map1.size)
-// Expected output: 2
+console.log(map1.size) // Expected output: 2
 
 // 2 Array and Map
 const map2 = new Map()
 map2.set('a', 1)
 map2.set('b', 2)
 const arr = [...map2]
-console.log(arr)
-// Expected output: Array [ [ 'a', 1 ], [ 'b', 2 ] ]
+console.log(arr) // Expected output: Array [ [ 'a', 1 ], [ 'b', 2 ] ]
